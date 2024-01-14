ff4bfdce029c02f072600dc0ec740f66e58db83f
Klajdi Hoxhalli <hoxhalli@sheridancollege.ca>
2024-01-13 21:12:18 -0500

    MOvement logic complete

--- Assets/Scripts/IsDestroyable.cs
+++ Assets/Scripts/IsDestroyable.cs
@@ -7,6 +7,26 @@ public class IsDestroyable : MonoBehaviour
     [SerializeField] private GameObject enemy;
     [SerializeField] private float scaleDecrease = 0.5f;
 
+    [SerializeField] private float speed = 1.0f;
+    private Vector3 direction;
+
+    void Start()
+    {
+        direction = new Vector3(Random.Range(-1.0f, 1.0f), Random.Range(-1.0f, 1.0f), Random.Range(-1.0f, 1.0f)).normalized;
+    }
+
+
+    void Update()
+    {
+        this.transform.position += direction * speed * Time.deltaTime;
+        
+        if(Mathf.Abs(this.transform.position.x) > 50.0f || Mathf.Abs(transform.position.y) > 50.0f
+           || Mathf.Abs(transform.position.z) > 50.0f)
+        {
+            direction *= -1;
+        }
+    }
+
     public void Split()
     {
         if(transform.localScale.x > 0.001f)
